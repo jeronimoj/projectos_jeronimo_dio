c91d89867084bd31039f4c5ec4a1aca68b19783b
Jeronimo <99995789+jeronimoj@users.noreply.github.com>
2022-03-12 23:06:15 +0100

    Add files via upload

--- desafio_de_codigo/home/dev4life/Desktop/media/src/Main.java
+++ desafio_de_codigo/home/dev4life/Desktop/media/src/Main.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            double A, B, media;
+
+            A = sc.nextDouble();
+            B = sc.nextDouble();
+
+
+            media = (   A   * 3.5 +   B  * 7.5)/11;
+
+            //TODO: Complete com a variável que representa o resultado da média.
+            System.out.printf("MEDIA = %.5f",     media);
+            System.out.println();
+        }
+    }
+
 
